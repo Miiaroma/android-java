784d2129e864aa19bf5bd864d9b0114179e221f7
Miiaroma <miia.romanainen@gmail.com>
2020-09-20 01:44:02 +0300

    Img button click, random generator and animation added.

--- androidJava/app/src/main/java/com/example/myapplication/Guess.java
+++ androidJava/app/src/main/java/com/example/myapplication/Guess.java
@@ -1,15 +1,104 @@
 package com.example.myapplication;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Guess extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Random;
+
+public class Guess extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "Guess activity";
+    ImageButton star;
+    ImageButton star1;
+    ImageButton star2;
+    ImageButton star3;
+    FloatingActionButton floatingActionButton;
+    Animation animation;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess);
+        star = findViewById(R.id.star);
+        star.setOnClickListener(this);
+        star1 = findViewById(R.id.star1);
+        star1.setOnClickListener(this);
+        star2 = findViewById(R.id.star2);
+        star2.setOnClickListener(this);
+        star3 = findViewById(R.id.star3);
+        star3.setOnClickListener(this);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(this);
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.roundanimation);
     }
 
+    public int getRandom() {
+        Random rnd = new Random();
+        return rnd.nextInt(4);
+    }
 
+    public void onClick(View view) {
+        int rnd = getRandom();
+
+        switch (view.getId()){
+            case R.id.star:
+                if(rnd == 0) {
+                    star.setVisibility(View.GONE);
+                    star.setImageResource(R.mipmap.ic_apple);
+                    star.setBackgroundColor(Color.WHITE);
+                    star.startAnimation(animation);
+                }
+                star.setVisibility(View.VISIBLE);
+
+                break;
+
+            case R.id.star1:
+                if(rnd == 1) {
+                    star1.setVisibility(View.GONE);
+                    star1.setImageResource(R.mipmap.ic_apple);
+                    star1.setBackgroundColor(Color.WHITE);
+                    star1.startAnimation(animation);
+                }
+                star1.setVisibility(View.VISIBLE);
+                break;
+
+            case R.id.star2:
+                if(rnd == 2) {
+                    star2.setVisibility(View.GONE);
+                    star2.setImageResource(R.mipmap.ic_apple);
+                    star2.setBackgroundColor(Color.WHITE);
+                    star2.startAnimation(animation);
+                }
+                star2.setVisibility(View.VISIBLE);
+                break;
+
+            case R.id.star3:
+                if(rnd == 3) {
+                    star3.setVisibility(View.GONE);
+                    star3.setImageResource(R.mipmap.ic_apple);
+                    star3.setBackgroundColor(Color.WHITE);
+                    star3.startAnimation(animation);
+                }
+                star3.setVisibility(View.VISIBLE);
+                break;
+            default:
+                break;
+        }
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(getIntent());
+            }
+        });
+    }
 }
